77a1fbc3e49b77acb32d9cd7cff8d670dab0daf9
Sourav Chatterjee <sromic@gmail.com>
2025-07-24 17:56:25 +0300

    Implement vertex deduplication with unordered_map and add hash specialization for Vertex.

--- Vulkan/src/VulkanRenderer.cpp
+++ Vulkan/src/VulkanRenderer.cpp
@@ -2,6 +2,7 @@
 
 #include <VulkanRenderer.hpp>
 #include <ShaderUtils.hpp>
+#include <unordered_map>
 
 #include <stdexcept>
 #include <iostream>
@@ -18,6 +19,8 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/hash.hpp>
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
@@ -67,7 +70,25 @@ struct Vertex
 
 		return attributeDescriptions;
 	}
+
+	bool operator==(const Vertex& other) const
+	{
+		return pos == other.pos && color == other.color && texCoord == other.texCoord;
+	}
 };
+
+namespace std
+{
+	template<> struct hash<Vertex>
+	{
+		size_t operator()(Vertex const& vertex) const
+		{
+			return ((hash<glm::vec3>()(vertex.pos) ^
+				(hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^
+				(hash<glm::vec2>()(vertex.texCoord) << 1);
+		}
+	};
+}
 
 struct UniformBufferObject
 {
@@ -788,6 +809,8 @@ void VulkanRenderer::loadModel()
 		throw std::runtime_error(error);
 	}
 
+	std::unordered_map<Vertex, uint32_t> uniqueVertices{};
+
 	for (const auto& shape : shapes) 
 	{
 		for (const auto& index : shape.mesh.indices) 
@@ -806,9 +829,13 @@ void VulkanRenderer::loadModel()
 
 			vertex.color = { 1.0f, 1.0f, 1.0f };
 
+			if (uniqueVertices.count(vertex) == 0)
+			{
+				uniqueVertices[vertex] = static_cast<uint32_t>(vertices.size());
+				vertices.push_back(vertex);
+			}
 
-			vertices.push_back(vertex);
-			indices.push_back(indices.size());
+			indices.push_back(uniqueVertices[vertex]);
 		}
 	}
 }
