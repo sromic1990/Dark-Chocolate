aaf65e10eee08052717dccce81bcc87b971d3f3c
Sourav Chatterjee <sromic@gmail.com>
2026-04-06 19:30:30 +0300

    Remove all files from project in order to fix lowercase/uppercase directory issue.

--- src/renderer/RenderCommand.cpp
+++
@@ -1,8 +0,0 @@
-// src/Renderer/RenderCommand.cpp
-#include "Renderer/RenderCommand.h"
-
-namespace DC
-{
-	// Static member must be defined in EXACTLY ONE .cpp file.
-	IRenderBackend* RenderCommand::s_Backend = nullptr;
-}
